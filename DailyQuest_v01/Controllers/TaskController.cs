03e869d9dc27a2ff89171763cddc7e37562affd4
Chloe <flykite159@yahoo.com.tw>
2025-05-22 22:07:02 +0800

    任務清單主頁頁面(尚缺<a><td>)

---
+++ DailyQuest_v01/Controllers/TaskController.cs
@@ -0,0 +1,19 @@
+﻿using DailyQuest_v01.Models;
+using Microsoft.AspNetCore.Mvc;
+using Task = DailyQuest_v01.Models.Task;
+
+namespace DailyQuest_v01.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly DailyQuestDbContext _db;
+        public TaskController(DailyQuestDbContext context) {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ DailyQuest_v01/Models/ViewModel/TaskTable.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DailyQuest_v01.Models.ViewModel
+{
+    public class TaskTable
+    {
+        [Display(Name = "任務類型")]
+        public string TaskTypeName { get; set; } = null!;
+        [Display(Name = "任務標籤")]
+        public string TaskLabelName { get; set; } = null!;
+        [Required(ErrorMessage = "任務內容必填")]
+        [Display(Name = "任務內容")]
+        public string TaskContent { get; set; } = null!;
+        [Display(Name = "子任務")]
+        public int SubTaskId { get; set; }
+        [Display(Name = "設定完成日")]
+        public DateTime ExpectDate { get; set; }
+        [Display(Name = "週期設定")]
+        public string? SetPeriod { get; set; }
+        [Display(Name = "發布時間")]
+        public DateTime CreateDate { get; set; }
+        [Display(Name = "完成日")]
+        public DateTime? FinishDate { get; set; }
+        [Display(Name = "任務結果")]
+        public string TaskResultName { get; set; } = null!;
+    }
+}
