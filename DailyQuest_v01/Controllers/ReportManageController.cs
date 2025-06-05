b78fa164942b8c5caaf4c1acc496d04ed1fd588b
Ada Chen <ch46234881@gmail.com>
2025-06-05 23:07:13 +0800

    新增檢舉處理頁面

---
+++ DailyQuest_v01/Controllers/ReportManageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DailyQuest_v01.Controllers
+{
+    public class ReportManageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return PartialView("~/Views/ReportManage/PartialViews/_ReportManageIndexPartial.cshtml");
+        }
+    }
+}
