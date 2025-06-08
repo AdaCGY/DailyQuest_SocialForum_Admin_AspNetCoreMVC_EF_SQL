19e6f73257bce24c60f82a043068c8fa8c16064b
Ada Chen <ch46234881@gmail.com>
2025-06-08 22:33:28 +0800

    被檢舉文章/留言資料顯示

--- DailyQuest_v01/Controllers/ReportManageController.cs
+++ DailyQuest_v01/Controllers/ReportManageController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DailyQuest_v01.Models;
+using DailyQuest_v01.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DailyQuest_v01.Controllers
 {
     public class ReportManageController : Controller
     {
-        public IActionResult Index()
+        private readonly DailyQuestDbContext _context;
+        public ReportManageController(DailyQuestDbContext context)
         {
-            return PartialView("~/Views/ReportManage/PartialViews/_ReportManageIndexPartial.cshtml");
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var viewModel = new ReportsManageViewModels
+            {
+                Reports = await _context.Reports
+                .Include(r => r.Post).Include(r => r.ReportCategory).Select(r => new ReportListViewModels
+                {
+                    ReportId = r.ReportId,
+                    MemberId = r.MemberId,
+                    TargetMemberId = r.Post.MemberId,
+                    ReportCategoryId = r.ReportCategoryId,
+                    ReportCategoryName = r.ReportCategory.ReportCategoryName,
+                    PostId = r.PostId,
+                    ReportedAt = r.ReportedAt,
+                    ReportContent = r.ReportContent,
+                    AdminId = r.AdminId,
+                    ProcessedAt = r.ProcessedAt,
+                    AdminComment = r.AdminComment,
+                    Status = r.Status,
+                    StatusName = r.Status == "Pending" ? "待處理"
+                    : r.Status == "Approved" ? "已處理"
+                    : r.Status == "Rejected" ? "檢舉未成立"
+                    : "狀態不明，請檢查"
+                }).ToListAsync()
+            };
+            return PartialView("~/Views/ReportManage/PartialViews/_ReportManageIndexPartial.cshtml",viewModel);
         }
     }
 }
---
+++ DailyQuest_v01/Models/ViewModels/ReportsManageViewModels.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DailyQuest_v01.Models.ViewModels
+{
+    public class ReportsManageViewModels
+    {
+        public IEnumerable<ReportListViewModels> Reports { get; set; } = null!;
+        
+    }
+}
